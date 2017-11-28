7b56fb33921cf1ce6ba8e7291b52b0f309bf7e59
Rebecca Strauss <rstrauss127@gmail.com>
2017-11-28 23:48:57 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,11 +2,11 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 index = 0
 
 def valid_move?(board, index)# code your #valid_move? method here
-((index >= 0) && (index <= 8) && !(position_taken?))
+((index >= 0) && (index <= 8) && (position_taken))
 
 end
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-def position_taken?(board, idex)
-  !(board[index] == "" || board[index] == " " || board [index] == nil)
+def position_taken(board, index)
+  (board[index] == "" || board[index] == " " || board [index] == nil)
 
 end
