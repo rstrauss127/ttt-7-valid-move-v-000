94336c538ed67e9adf2f3918fe3c324bba3f8383
Rebecca Strauss <rstrauss127@gmail.com>
2017-11-29 00:05:17 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,11 @@
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+index = 0
+
+def valid_move?(board, index)
+ (position_taken?) && (index >= 0) && (index <= 8)
+end
 
 def position_taken?(board, index)
   (board[index] == "" || board[index] == " " || board [index] == nil)
 
-end
-
-def valid_move?(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "], index = 0)
- (position_taken?) && (index >= 0) && (index <= 8)
 end
